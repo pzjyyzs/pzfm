3306cc3e75f6beca1cc4e3834c2c4f2ad024e674
pzjyyzs <463669395@qq.com>
2021-03-03 23:03:07 +0800

    breadcrumb

--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -2,16 +2,18 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './layouts/header/header.component';
+import { BreadcrumbModule } from './share/components/breadcrumb/breadcrumb.module';
 
 
 
 @NgModule({
   declarations: [HeaderComponent],
   imports: [
-  BrowserModule,
+    BrowserModule,
+    BreadcrumbModule,
     AppRoutingModule
   ],
-  exports: [HeaderComponent]
+  exports: [HeaderComponent, BreadcrumbModule]
 })
 export class CoreModule {
   constructor(@SkipSelf() @Optional() parentModule: CoreModule) {
---
+++ src/app/share/components/breadcrumb/breadcrumb.component.ts
@@ -0,0 +1,19 @@
+
+import { Component, OnInit, ChangeDetectionStrategy, ViewEncapsulation, TemplateRef, Input } from '@angular/core';
+
+@Component({
+  selector: 'fm-breadcrumb',
+  templateUrl: './breadcrumb.component.html',
+  styleUrls: ['./breadcrumb.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
+})
+export class BreadcrumbComponent implements OnInit {
+
+  @Input() fmSeparator: TemplateRef<any>;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
