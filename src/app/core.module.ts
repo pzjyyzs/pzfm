c45ea691b344a8c112a41bb3eb63058624e24300
pzjyyzs <463669395@qq.com>
2021-04-10 16:26:50 +0800

    头部动画

--- src/app/core.module.ts
+++ src/app/core.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './layouts/header/header.component';
 import { PagesModule } from './pages/pages.module';
@@ -12,6 +13,7 @@ import { BreadcrumbModule } from './share/components/breadcrumb/breadcrumb.modul
   declarations: [HeaderComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     BreadcrumbModule,
     PagesModule,
--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,18 +1,58 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { DOCUMENT } from '@angular/common';
+import { transformAll } from '@angular/compiler/src/render3/r3_ast';
+import { Component, OnInit, ChangeDetectionStrategy, Inject, ChangeDetectorRef, AfterViewInit, ElementRef } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { User } from 'src/app/services/types';
 
 @Component({
   selector: 'fm-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('moveUpMotion', [
+      state('true', style({
+        position: 'fixed',
+        top: 0,
+        width: '100%',
+        zIndex: 1,
+      })),
+      transition('* => true', [
+        style({
+          transform: 'translateY(-100%)',
+          opacity: 0
+        }),
+        animate('300ms ease-out')
+      ])
+    ])
+  ]
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
 
   user: User;
-  constructor() { }
+  fix = false;
+  constructor(
+    @Inject(DOCUMENT) private doc: Document,
+    private el: ElementRef,
+    private cdr: ChangeDetectorRef
+  ) { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    fromEvent(this.doc, 'scroll')
+    .pipe(debounceTime(300), distinctUntilChanged())
+    .subscribe(() => {
+      const top = this.doc.documentElement.scrollTop;
+      if (top > this.el.nativeElement.clientHeight + 100) {
+        this.fix = true;
+      } else if (top === 0) {
+        this.fix = false;
+      }
+      this.cdr.markForCheck();
+    });
+  }
 }
